88663c3e567e27f4be2d6b405d0b4a56e52cafb1
Kostya <mr.ratusha@gmail.com>
2017-08-11 16:14:24 +0300

    added `deny` endpoint

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -163,6 +163,54 @@ router.post('/friends/cancel/:userId', async (ctx, next) => {
   }
 });
 
+router.post('/friends/deny/:userId', async (ctx, next) => {
+  try {
+
+    const {_id} = jwt.verify(ctx.request.header.authorization, 'secret').user;
+    const user = await User.findOne({_id});
+    const {userId} = ctx.params;
+
+    await new Promise((resolve, reject) => {
+      try {
+        if (_id === userId) {
+          ctx.body = {
+            errors: {
+              detail: 'You can\'t make friend request to yourself',
+            },
+          };
+        } else {
+
+          user.denyRequest(userId, (err, res) => {
+            if (err) {
+              ctx.body = {
+                detail: 'Error: A pending request already exists',
+              };
+              reject();
+              console.error(err);
+            } else {
+
+              ctx.body = {
+                dateSent: res.dateSent,
+                status: res.status,
+                _id: res._id,
+              };
+              resolve();
+            }
+          });
+        }
+      } catch (error) {
+        ctx.body = {
+          detail: 'Error',
+        };
+        console.log(error);
+        reject();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get('/friends/get', async (ctx, next) => {
   await new Promise((resolve, reject) => {
     try {
